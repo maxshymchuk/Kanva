e6bd4dd9d8a9ca398fc2dcfc6ad25e51e87b533e
Max Shymchuk <octorix@mail.ru>
2021-04-02 01:43:41 +0300

    Updates

---
+++ precache-manifest.b342af32dc49fd8fb5e379c64fc43ea1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64686c91b9b80da6f8e627d436cf159",
+    "url": "/Kanva/index.html"
+  },
+  {
+    "revision": "51d5fb9dadd7e4faa686",
+    "url": "/Kanva/static/css/main.c7a5e9f4.chunk.css"
+  },
+  {
+    "revision": "6cb1ed642dcc0b79af68",
+    "url": "/Kanva/static/js/2.78bcde1e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Kanva/static/js/2.78bcde1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51d5fb9dadd7e4faa686",
+    "url": "/Kanva/static/js/main.411ea0e7.chunk.js"
+  },
+  {
+    "revision": "2c09766d07ae24b92d61",
+    "url": "/Kanva/static/js/runtime-main.2c1a89ed.js"
+  },
+  {
+    "revision": "41ae3b1f9400c1c3cd13cf038fa9bfb2",
+    "url": "/Kanva/static/media/delete.41ae3b1f.svg"
+  },
+  {
+    "revision": "bf07a22359b7d447c9f93ab77f4ad700",
+    "url": "/Kanva/static/media/export.bf07a223.svg"
+  },
+  {
+    "revision": "715ec00e6cfba4dcde5019f56fd496a5",
+    "url": "/Kanva/static/media/import.715ec00e.svg"
+  }
+]);
